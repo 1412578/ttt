3d7629e5bf8d6d5b1324ec4d4aebd1c5052b9ac1
Trần Quang Tri <tranquangtri96v2@gmail.com>
2018-06-02 04:00:04 +0700

    Thêm inline bundlecss

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.s?css$/,
-				use: ["style-loader", "css-loader", "sass-loader"]
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
 			}
 		]
 	}

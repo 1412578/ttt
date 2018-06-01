2969ad61f65fe5199ba23c34cd57f5ef819c4105
Trần Quang Tri <tranquangtri96v2@gmail.com>
2018-06-02 03:15:34 +0700

    Thêm purify css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require("path");
+const glob = require('glob');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurifyCSSPlugin = require('purifycss-webpack');
 module.exports = {
 	mode: "development",
 	entry: {
@@ -15,6 +17,10 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: "[name].bundle.css"
+    }),
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'views/layout.hbs')),
     })
   	],
 	module: {

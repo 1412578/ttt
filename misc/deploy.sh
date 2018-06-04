d42b264d92cbcbde7458c52201c32815de599dcc
Trần Quang Tri <tranquangtri96v2@gmail.com>
2018-06-05 03:52:57 +0700

    g

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -6,7 +6,7 @@ ssh-add .travis/deploy # Add the private key to SSH
 git show-branch
 git config --global push.default matching
 git remote add deploy ssh://deploy@$IP$DEPLOY_DIR
-git push deploy HEAD
+git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh deploy@$IP <<EOF

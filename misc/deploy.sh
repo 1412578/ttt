3dc6689493f7640826ff2baad8f6d996f6f2f777
Trần Quang Tri <tranquangtri96v2@gmail.com>
2018-06-05 02:55:23 +0700

    push current branch

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -5,7 +5,7 @@ chmod 600 .travis/deploy # Allow read access to the private key
 ssh-add .travis/deploy # Add the private key to SSH
 
 git remote add deploy ssh://git@$IP$DEPLOY_DIR
-git push deploy html_to_hbs
+git push deploy HEAD
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh deploy@$IP <<EOF
